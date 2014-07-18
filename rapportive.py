da09ecd44cffce5e714b6fd2392748e954bb3c2a
SudhanshuC <sudhanshuchauhan007@yahoo.com>
2014-07-18 18:01:40 +0530

    Update rapportive.py

--- rapportive.py
+++ rapportive.py
@@ -3,7 +3,8 @@
 #Credits: This code is based upon the research and code of Jordan Wright.
 #Blog Link: http://jordan-wright.github.io/blog/2013/10/14/automated-social-engineering-recon-using-rapportive/
 #Code Link: https://github.com/jordan-wright/rapportive
-# This code requires the Requests library: https://pypi.python.org/pypi/requests/
+#This code requires the Requests library: https://pypi.python.org/pypi/requests/
+# Rapportive Code: https://github.com/SudhanshuC/Rapportive/blob/master/rapportive.py
 import requests
 target_email = raw_input("Enter Target Email: ")
 random_email="randomemail@gmail.com"
